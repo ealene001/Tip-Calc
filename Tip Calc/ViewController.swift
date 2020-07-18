4297def1098f9b5c30129d058e2a9219b9ab59b9
ealene001 <estifanos.alene@gmail.com>
2020-07-17 21:09:22 -0500

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Estifanos Alene on 7/17/20.
+//  Copyright © 2020 Estifanos Alene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
