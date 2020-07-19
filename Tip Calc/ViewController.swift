16b1b115aa45b7874fd9c2aa2d7249c5922dc9d5
ealene001 <estifanos.alene@gmail.com>
2020-07-19 00:41:43 -0500

    Change UI / Add Slider for Split and Tip

--- Tip Calc/ViewController.swift
+++ Tip Calc/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var numOfPeople: UITextField!
+    
+    @IBOutlet weak var splitBill: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get inital bill amount and calculate tip
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        // Calculate tip and total amount
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        
+        // Update view in app
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        // Splitting the Bill
+        let people = Double(numOfPeople.text!) ?? 1
+        let finalBill = total / people
+        splitBill.text = String(format: "$%.2f", finalBill)
+    }
+    
+  
+    @IBAction func tapButton(_ sender: Any) {
+        performSegue(withIdentifier: "nextPage", sender: self)
+    }
+    
 }
 
